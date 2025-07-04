0c7777238d2619075baa90ed47ed9e6139c0c53f
NguyenTan205 <nguyenvantan.utc@gmail.com>
2025-07-04 11:37:47 +0700

    first commit

---
+++ Variable-Datatype/VariableDatatype.java
@@ -0,0 +1,16 @@
+public class VariableDatatype {
+    public static void main(String[] args) {
+        int i = 10; // khai bao bien i la kieu int co gia tri la 10
+        float f = 20.5f; // khai bao bien f la kieu float co gia tri la 20.5
+        double d = 20.5; // khai bao bien d la kieu double co gia tri la 20.5
+        boolean b = true; // khai bao bien b la kieu boolean co gia tri la true
+        char c = 'a'; // khai bao bien c co kieu la char co gia tri la ki tu a
+        String s = "Hà Nội"; // khai bao bien s co kieu la string co gia tri la chuoi Ha noi
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
