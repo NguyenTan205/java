e045aaacdbb30c134250237b92e38c07f3536a6c
NguyenTan205 <nguyenvantan.utc@gmail.com>
2025-07-07 11:01:47 +0700

    tien lai

--- interest-calculator/Interest.java
+++ interest-calculator/Interest.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Interest {
+    public static void main(String[] args) {
+        double money = 1.0;
+        int month = 1;
+        double interesta = 1.0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhập vào số tiền gửi: ");
+        money = input.nextDouble();
+        System.out.println("Nhập vào số tháng gửi: ");
+        month = input.nextInt();
+        System.out.println("Nhập vào lãi suất: ");
+        interesta = input.nextDouble();
+        double totalInterest = 0;
+        for (int i = 0; i < month; i++) {
+            totalInterest += money * (interesta / 100) / 12 * month;
+        }
+        System.out.println("Tổng số tiền lãi là: " + totalInterest);
+    }
 }
