b5efccf62e2e36948d3bd49fe307afc92f4cfd7a
NguyenTan205 <nguyenvantan.utc@gmail.com>
2025-07-10 14:23:18 +0700

    lop

---
+++ xay-dung-lop-chi-ghi/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setClasses("C05");
+        student.setName("Tan");
+    }
+}
