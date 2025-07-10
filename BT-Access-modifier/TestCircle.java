47302e2b289e1934407ff1c588e88dedef8e8738
NguyenTan205 <nguyenvantan.utc@gmail.com>
2025-07-10 14:09:01 +0700

    Access-modifier

---
+++ BT-Access-modifier/TestCircle.java
@@ -0,0 +1,8 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        double radius = circle.getRadius();
+        double area = circle.getArea();
+        System.out.println("Dien tich hinh tron la: " + area);
+    }
+}
