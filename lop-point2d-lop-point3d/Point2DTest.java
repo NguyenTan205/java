5435cfe24ba842bea78ae5214d141fc894facafe
NguyenTan205 <nguyenvantan.utc@gmail.com>
2025-07-11 16:29:07 +0700

    point2D

---
+++ lop-point2d-lop-point3d/Point2DTest.java
@@ -0,0 +1,8 @@
+public class Point2DTest {
+    public static void main(String[] args) {
+        Point2D p1 = new Point2D(0,0);
+        System.out.println(p1);
+        Point2D p2 = new Point2D(1,1);
+        System.out.println(p2);
+    }
+}
---
+++ lop-point2d-lop-point3d/Point3DTest.java
@@ -0,0 +1,6 @@
+public class Point3DTest {
+    public static void main(String[] args) {
+        Point3D p1 = new Point3D(1, 1, 1);
+        System.out.println(p1);
+    }
+}
